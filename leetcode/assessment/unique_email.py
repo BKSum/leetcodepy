d8183cdc939ba543ddf74db3557063c7e71bb468
Brandon <brandon.sum@gmail.com>
2021-06-20 14:35:05 -0400

    add

---
+++ leetcode/assessment/unique_email.py
@@ -0,0 +1,29 @@
+from typing import List
+import re
+
+class UniqueEmail:
+    def handleDot(self, email: str) -> str:
+        indexAt = email.index("@")
+        beforeAt = email[:indexAt]
+        afterAt = email[indexAt:]
+        return beforeAt.replace(".", "") + afterAt
+
+    def handleFirstPlus(self, email: str) -> str:
+        try:
+            plusIndex = email.index("+")
+        except ValueError as ve:
+            return email
+        indexAt = email.index("@")
+        beforeAt = email[:indexAt]
+        afterAt = email[indexAt:]
+        beforePlus = beforeAt[:plusIndex]
+        return beforePlus + afterAt
+
+    def actualEmail(self, email: str) -> str:
+        return self.handleDot(self.handleFirstPlus(email))
+
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        outputSet = set()
+        for email in emails:
+            outputSet.add(self.actualEmail(email))
+        return len(outputSet)
